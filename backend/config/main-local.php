935864443d7aa7c604a6d038fe9a2f8bb5b20d81
Imran Omar Bukhsh <imranomar@gmail.com>
2018-10-13 11:43:44 +0400

    .gitignore edited

---
+++ backend/config/main-local.php
@@ -0,0 +1,28 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'NBb9POuU5S7ZAyWihxAkRMLrJ_a2Y39U',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
